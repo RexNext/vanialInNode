7c9c89d29cabcfe83a6c5b7420d24d238895e8a4
RexNext box <rexnext@gmail.com>
2020-11-10 00:03:06 +0000

    add let

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var diff =require("./DateLib/diff.js") ;
-var over=require("./DateLib/isTimeUp.js");
+let diff =require("./DateLib/diff.js") ;
+let over=require("./DateLib/isTimeUp.js");
 
-var mynow=new Date();
+let mynow=new Date();
 console.log(mynow);
 
-var myexp=new Date("2020-11-09 22:00:00");
+let myexp=new Date("2020-11-10 22:00:00");
 console.log(myexp);
 
-e=diff(myexp,mynow);
+let e=diff(myexp,mynow);
 // console.log(e.m);
 // console.log(e.h);
 console.log(`${e.sign} ${e.d} Day, ${e.h} houre and ${e.m} minute`);
 
-t=over(myexp,mynow);
+let t=over(myexp,mynow);
 console.log(t.status?"time is over":`remaning time is ${t.d} day , ${t.h} houre ,${t.m} minute`);
